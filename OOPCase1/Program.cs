f4fd3d78f4ae297f920b8f070b3dd08ca122318b
EnKop420 <rasmushww@gmail.com>
2022-11-22 03:24:42 +0100

    Fixed StudentId

--- OOPCase1/Program.cs
+++ OOPCase1/Program.cs
@@ -1,10 +1,10 @@
 ﻿Student amanda = new(1, "Amanda", "Gudmand", new DateOnly(1990, 1, 1)) ;
-Student alexander = new(1, "Alexander", "Runge", new DateOnly(2005, 2, 5));
-Student ozan = new(1, "Ozan", "Korkmaz", new DateOnly(1999, 12, 4));
-Student camilla = new(1, "Camilla", "Kløjgaard", new DateOnly(1890, 4, 4));
-Student dennis = new(1, "Dennis", "Paaske", new DateOnly(1980, 3, 4));
+Student alexander = new(2, "Alexander", "Runge", new DateOnly(2005, 2, 5));
+Student ozan = new(3, "Ozan", "Korkmaz", new DateOnly(1999, 12, 4));
+Student camilla = new(4, "Camilla", "Kløjgaard", new DateOnly(1890, 4, 4));
+Student dennis = new(5, "Dennis", "Paaske", new DateOnly(1980, 3, 4));
 
-Teacher niels = new("Tortur", "Niels", "Olesen", new DateOnly(1971, 1, 23));
+Teacher niels = new("Lærer", "Niels", "Olesen", new DateOnly(1971, 1, 23));
 
 Course grundlæggende = new("Grundelæggende Programmering", niels);
 Course studieteknik = new("Studieteknik", niels );
